846297006cc4a411589acd351702a64a0677eb04
salaam <abdisalamxhassan@gmail.com>
2025-03-12 07:52:50 +0300

    iterating tuples

---
+++ PYTHON/WEEK 2/ITERATE_TUPLE.py
@@ -0,0 +1,14 @@
+"""
+2. Create a tuple containing the names of five of your favorite books. Then, use a for loop to print each book name on a separate line.
+"""
+
+books = (
+    "48 LAWS OF POWER",
+    "RICH DAD, POOR DAD",
+    "ATOMIC HABITS",
+    "12 RULES OF LIFE",
+    "The 7 Habits of Highly Effective People",
+)
+
+for book in books:
+    print(book)
